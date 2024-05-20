ae9a2a652fc7c6bc6a2b8bef0d044d1268793236
riqsans <thrq88@gmail.com>
2024-05-20 15:33:50 +0700

    menuliskan int main

--- Agregasi/anak.h
+++ Agregasi/anak.h
@@ -1,14 +1,18 @@
 #ifndef ANAK_H
 #define ANAK_H
 
-class anak {
-public:
-    string name;
-    anak(string pNama) :nama(pNama){
+class anak
+{
+    public:
+    string nama;
+    anak(string pNama) : nama(pNama)
+    {
         cout << "Anak \"" << nama << "\" ada\n";
     }
-    ~anak() {
-        cout << "Anak \"" << nama << "\" tidak ada\n";
+
+    ~anak()
+    {
+        cout << "Anak \"" << nama << "\" tidak ada\n"; 
     }
 };
 #endif
