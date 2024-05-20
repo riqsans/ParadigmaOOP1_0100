b00c500e9293d11f2280c79a9c78ba9e9bdaf1d4
riqsans <thrq88@gmail.com>
2024-05-20 18:15:21 +0700

    menuliskan akses modifier public untuk class dokter

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -10,8 +10,20 @@ public:
     pasien(string pNama) :nama(pNama){
         cout << "Pasien \"" << nama << "\" ada\n";
     }
-~pasien(){
+    ~pasien(){
     cout << "Pasien \"" << nama << "\" tidak ada\n";
-}
-void tambahDokter(dokter*);
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
+class dokter{
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+
+    
 }
