336246317a9356c9a26a0c3cadd75bc3e37236e5
riqsans <thrq88@gmail.com>
2024-05-20 18:08:10 +0700

    membuat class dokter dan class pasien

--- Komposisi/manusia.h
+++ Komposisi/manusia.h
@@ -3,16 +3,17 @@
 
 using namespace std;
 
-class manusia {
-public:
-    string name;
-    jantung varJantung;
+class manusia
+{
+    public:
+        string name;
+        jantung varJantung;
 
-    manusia(string pName)
-        : name(pName)
+        manusia(string pName) : name(pName)
         {
             cout << name << " hidup\n";
         }
+        
         ~manusia()
         {
             cout << name << "mati\n";
