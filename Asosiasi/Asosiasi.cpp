51e20aa4646572f97e5ad9fbb5cfa45d32ca9d9c
riqsans <thrq88@gmail.com>
2024-05-20 18:12:10 +0700

    menuliskan void tambah dokter

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -10,5 +10,8 @@ public:
     pasien(string pNama) :nama(pNama){
         cout << "Pasien \"" << nama << "\" ada\n";
     }
-
+~pasien(){
+    cout << "Pasien \"" << nama << "\" tidak ada\n";
+}
+void tambahDokter(dokter*);
 }
